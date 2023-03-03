113b791ac9694e53b2c86b738507a3b4c344d3df
pouyaaghayi <pouyaeyjey@gmail.com>
2023-03-04 02:28:40 +0330

    add product img and update offer code box

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s black',
+      }
+    },
   },
 variants: {
   extend: {
