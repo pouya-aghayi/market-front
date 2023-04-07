f036fb6b1cf7127d5b5e15f9c8e20a70f4845f7b
pouyaaghayi <pouyaeyjey@gmail.com>
2023-04-07 16:13:44 +0430

    responsive for ipad air

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
+     screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'ipadair':'820px',
+      // => @media (min-width: 820px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1280px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       animation: {
         'spin-slow': 'spin 3s black',
